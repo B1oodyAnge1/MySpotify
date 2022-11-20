77926a6ebe4c06b3c3dfcfcb3dde6ed0ab0be5a7
B1oodyAnge1 <romsm@bk.ru>
2022-11-21 01:55:38 +0300

    Вступительные изменения

---
+++ lib/start.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class Spotify extends StatelessWidget {
+  const Spotify({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color.fromRGBO(255, 255, 255, 1),
+      child: const Text("Hellow"),
+    );
+  }
+}
